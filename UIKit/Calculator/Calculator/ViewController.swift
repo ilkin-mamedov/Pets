793d99eda910bee9bf781b8e2d891b0f0b694e6e
Ilkin Mamedov <13katyukhin@gmail.com>
2022-04-06 23:22:21 +0300

    Added New Calculator App

---
+++ UIKit/Calculator/Calculator/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func numberPressed(_ sender: UIButton) {
+    }
+}
