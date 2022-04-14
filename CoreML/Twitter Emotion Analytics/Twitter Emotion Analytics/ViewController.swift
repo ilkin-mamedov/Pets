cdbd9cc81fc81e8443ad9aea7358a83c1c4b3382
Ilkin Mamedov <13katyukhin@gmail.com>
2022-04-14 23:30:03 +0300

    Added New Twitter Emotion Analytics App

---
+++ CoreML/Twitter Emotion Analytics/Twitter Emotion Analytics/ViewController.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var emotionImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func predictPressed(_ sender: UIButton) {
+        
+    }
+}
