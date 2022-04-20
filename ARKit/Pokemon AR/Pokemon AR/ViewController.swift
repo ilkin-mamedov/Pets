2454c74e1fc943a35e138cf9d9ff7f939f046faf
Ilkin Mamedov <13katyukhin@gmail.com>
2022-04-20 10:03:52 +0300

    Added New Pokemon AR App

---
+++ ARKit/Pokemon AR/Pokemon AR/ViewController.swift
@@ -0,0 +1,34 @@
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackImages = ARReferenceImage.referenceImages(inGroupNamed: "Pokemon Cards", bundle: Bundle.main) {
+            configuration.trackingImages = trackImages
+            configuration.maximumNumberOfTrackedImages = 1
+            print("Images successfully added")
+        }
+        
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        sceneView.session.pause()
+    }
+}
